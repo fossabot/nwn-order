5cc69339b3572b92f07ca36c9444af57d2bc7fe8
urothis <natecaple@gmail.com>
2018-11-10 10:40:45 +0000

    Still not anywhere close to done, will flip this over to public.

--- go/main.go
+++ go/main.go
@@ -2,52 +2,144 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
+	"os"
 
-	"github.com/google/go-github/github"
+	"github.com/go-redis/redis"
+	"github.com/jasonlvhit/gocron"
 )
 
-func handleWebhook(w http.ResponseWriter, r *http.Request) {
-	config, _ := LoadConfiguration("config.json")
-	payload, err := github.ValidatePayload(r, []byte(config.External.GithubWebhookSecret))
-	if err != nil {
-		log.Printf("error validating request body: err=%s\n", err)
-		return
+func oneminutetask() {
+
+	client := redis.NewClient(&redis.Options{
+		Addr: "redis:" + os.Getenv("NWN_ORDER_REDIS_PORT"),
+	})
+	defer client.Close()
+
+	pubsub := client.Subscribe("heartbeat")
+	defer pubsub.Close()
+
+	if err := client.Publish("heartbeat", "1minute").Err(); err != nil {
+		panic(err)
 	}
-	defer r.Body.Close()
 
-	event, err := github.ParseWebHook(github.WebHookType(r), payload)
-	if err != nil {
-		log.Printf("could not parse webhook: err=%s\n", err)
-		return
+	fmt.Println("Pubsub sent: 1 Minute heartbeat")
+}
+
+func fiveminutetask() {
+
+	client := redis.NewClient(&redis.Options{
+		Addr: "redis:" + os.Getenv("NWN_ORDER_REDIS_PORT"),
+	})
+	defer client.Close()
+
+	pubsub := client.Subscribe("heartbeat")
+	defer pubsub.Close()
+
+	if err := client.Publish("heartbeat", "5minute").Err(); err != nil {
+		panic(err)
 	}
 
-	switch e := event.(type) {
+	fmt.Println("Pubsub sent: 5 Minute heartbeat")
+}
 
-	case *github.PushEvent:
-		go sendModuleUpdate()
-		fmt.Printf("%s made a commit to repo %s\n",
-			*e.Sender.Login, *e.Repo.FullName)
+func thirtyminutetask() {
 
-	default:
-		log.Printf("unknown event type %s\n", github.WebHookType(r))
-		return
+	client := redis.NewClient(&redis.Options{
+		Addr: "redis:" + os.Getenv("NWN_ORDER_REDIS_PORT"),
+	})
+	defer client.Close()
+
+	pubsub := client.Subscribe("heartbeat")
+	defer pubsub.Close()
+
+	if err := client.Publish("heartbeat", "30minute").Err(); err != nil {
+		panic(err)
 	}
+
+	fmt.Println("Pubsub sent: 30 Minute heartbeat")
+}
+
+func onehourtask() {
+
+	client := redis.NewClient(&redis.Options{
+		Addr: "redis:" + os.Getenv("NWN_ORDER_REDIS_PORT"),
+	})
+	defer client.Close()
+
+	pubsub := client.Subscribe("heartbeat")
+	defer pubsub.Close()
+
+	if err := client.Publish("heartbeat", "1hour").Err(); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Pubsub sent: 1 Hour heartbeat")
+}
+
+func sixhourtask() {
+
+	client := redis.NewClient(&redis.Options{
+		Addr: "redis:" + os.Getenv("NWN_ORDER_REDIS_PORT"),
+	})
+	defer client.Close()
+
+	pubsub := client.Subscribe("heartbeat")
+	defer pubsub.Close()
+
+	if err := client.Publish("heartbeat", "6hour").Err(); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Pubsub sent: 6 Hour heartbeat")
+}
+
+func twelvehourtask() {
+
+	client := redis.NewClient(&redis.Options{
+		Addr: "redis:" + os.Getenv("NWN_ORDER_REDIS_PORT"),
+	})
+	defer client.Close()
+
+	pubsub := client.Subscribe("heartbeat")
+	defer pubsub.Close()
+
+	if err := client.Publish("heartbeat", "12hour").Err(); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Pubsub sent: 12 Hour heartbeat")
+}
+
+func twentyfourhourtask() {
+
+	client := redis.NewClient(&redis.Options{
+		Addr: "redis:" + os.Getenv("NWN_ORDER_REDIS_PORT"),
+	})
+	defer client.Close()
+
+	pubsub := client.Subscribe("heartbeat")
+	defer pubsub.Close()
+
+	if err := client.Publish("heartbeat", "24hour").Err(); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Pubsub sent: 24 Hour heartbeat")
 }
 
 func main() {
-	//config, _ := LoadConfiguration("config.json")
+	fmt.Println("Order has started")
 
-	//consensus := externalip.DefaultConsensus(nil, nil)
-	//ip, _ := consensus.ExternalIP()
+	fmt.Println("NWN_ORDER_REDIS_PORT:", os.Getenv("NWN_ORDER_REDIS_PORT"))
 
-	//fmt.Println("webserver started | external IP of " + ip.String() + ":" + config.Webserver.Port)
+	gocron.Every(1).Minute().Do(oneminutetask)
+	gocron.Every(5).Minutes().Do(fiveminutetask)
+	gocron.Every(30).Minutes().Do(thirtyminutetask)
 
-	//http.HandleFunc("/webhook", handleWebhook)
-	//log.Fatal(http.ListenAndServe(":8080", nil))
+	gocron.Every(1).Hour().Do(onehourtask)
+	gocron.Every(6).Hours().Do(sixhourtask)
+	gocron.Every(12).Hours().Do(twelvehourtask)
+	gocron.Every(24).Hours().Do(twentyfourhourtask)
 
-	//go sendNwnxeeUpdate()
-
-	fmt.Println(nwnxeeImageCheck)
+	<-gocron.Start()
 }
